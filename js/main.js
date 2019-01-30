0cd359b3428b1187b8467bfeb4b0c440959be67d
Ivan-Kokorin <cannibal_irk@mail.ru>
2019-01-31 07:19:23 +0800

    #add: добавлена вёрстка всех частей футера

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(document).ready( function() {
         arrows: false,
         dots: true,
         slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToScroll: 1
     });
+    $('.costamer-testimonials__slider').slick({
+    	arrows: true,
+    	dots: false,
+    	idesToShow: 1,
+        slidesToScroll: 1
+    })
 });
